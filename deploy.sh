ae9110f13984650c191f0ebc5eb775659782878e
bharath-locha <velubharath9@gmail.com>
2025-07-04 18:35:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
 echo "Starting deployment..."
-sudo pkill -f app.py
+
+# Navigate to project directory
+cd /home/ec2-user/Cloud_Devops || exit 1
+
+# Kill previous Flask app if running
+sudo pkill -f app.py || true
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Start Flask app
 nohup python3 app.py > output.log 2>&1 &
+
 echo "Deployment completed."
